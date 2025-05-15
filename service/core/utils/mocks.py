35997ff96112d5e06687d94018f957f32fb7f47f
milkywayisdead <existentiary@gmail.com>
2025-05-15 16:32:06 +0300

    fix

--- service/core/utils/mocks.py
+++ service/core/utils/mocks.py
@@ -1,7 +1,7 @@
 HOSTS = [
     {
         '_id': '67a63d4dff2252cb4dd62d04',
-        'address': '1307891573',
+        'address': 1307891573,
         'IP': '77.244.215.117',
         'ASN': 'AS50340',
         'Location': [{ 'country': 'Russia', 'city': 'Moscow' }],
@@ -25,13 +25,45 @@ HOSTS = [
             {
                 'port': '80/tcp',
                 'service': 'http',
-                'software': ' Apache/2.4.18 (Ubuntu)',
-                'application': 'Shopify',
-                'component': 'None',
-                'title': '',
-                'headers': '',
-                'body': ''
-            }
+                'software': 'nginx/1.18.0 (Ubuntu)',
+                'application': 'Drupal',
+                'component': 'Redis',
+                'title': 'Welcome to nginx!',
+                'headers': 'HTTP/1.1 200 OK\r\n' +
+                  'Server: nginx/1.18.0 (Ubuntu)\r\n' +
+                  'Date: Tue, 13 May 2025 11:32:10 GMT\r\n' +
+                  'Content-Type: text/html\r\n' +
+                  'Content-Length: 612\r\n' +
+                  'Last-Modified: Wed, 04 Dec 2024 14:58:31 GMT\r\n' +
+                  'Connection: close\r\n' +
+                  'ETag: "67506e17-264"\r\n' +
+                  'Accept-Ranges: bytes',
+                'body': '<!DOCTYPE html>\n' +
+                  '<html>\n' +
+                  '<head>\n' +
+                  '<title>Welcome to nginx!</title>\n' +
+                  '<style>\n' +
+                  '    body {\n' +
+                  '        width: 35em;\n' +
+                  '        margin: 0 auto;\n' +
+                  '        font-family: Tahoma, Verdana, Arial, sans-serif;\n' +
+                  '    }\n' +
+                  '</style>\n' +
+                  '</head>\n' +
+                  '<body>\n' +
+                  '<h1>Welcome to nginx!</h1>\n' +
+                  '<p>If you see this page, the nginx web server is successfully installed and\n' +
+                  'working. Further configuration is required.</p>\n' +
+                  '\n' +
+                  '<p>For online documentation and support please refer to\n' +
+                  '<a href="http://nginx.org/">nginx.org</a>.<br/>\n' +
+                  'Commercial support is available at\n' +
+                  '<a href="http://nginx.com/">nginx.com</a>.</p>\n' +
+                  '\n' +
+                  '<p><em>Thank you for using nginx.</em></p>\n' +
+                  '</body>\n' +
+                  '</html>\n'
+              }
         ]
     }
 ]
--- ui/src/components/search/cards/PortInfoCard.vue
+++ ui/src/components/search/cards/PortInfoCard.vue
@@ -1,10 +1,20 @@
 <template>
 <v-card 
-    :title="`${locale.search.results.port} ${info.port}`">
+    :title="`${info.port}`">
     <v-card-text>
-        <p v-for="param in mainParams" :key="param.value">
-            {{ locale.port[param.title] }}: {{ info[param.value] }}
-        </p>
+        <p v-if="info.title" style="font-size:1rem;font-weight:600">{{ info.title }}</p>
+        
+        <div v-if="body.length" class="pl-0 pt-2 mt-2">
+            <p v-for="(el, index) in body">
+                {{ el }}
+            </p>
+        </div>
+
+        <div v-if="headers.length" class="v-card-subtitle pl-0 pt-2 mt-2">
+            <p v-for="(header, index) in headers">
+                {{ header }}
+            </p>
+        </div>
     </v-card-text>      
 </v-card>
 </template>
@@ -44,6 +54,16 @@ export default {
             return this._mainParams.filter(
                 item => this.info[item.value] !== ''
             )
+        },
+        headers(){
+            const headers = this.info.headers
+            if(!headers) return headers
+            return headers.split('\r\n')
+        },
+        body(){
+            const body = this.info.body
+            if(!body) return body
+            return body.split('\n')
         }
     }
 }
